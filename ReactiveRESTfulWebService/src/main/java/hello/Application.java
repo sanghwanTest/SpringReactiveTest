7b80df6392a9ccccad38d0160274e8135e28d6ef
이상환 <@DESKTOP-4J3RAR0>
2018-11-27 23:27:34 +0900

    master up 6

--- ReactiveRESTfulWebService/src/main/java/hello/Application.java
+++ ReactiveRESTfulWebService/src/main/java/hello/Application.java
@@ -10,6 +10,7 @@ public class Application {
 
 		GreetingWebClient gwc = new GreetingWebClient();
 		System.out.println("aaa");
+		System.out.println("bbb");
 		System.out.println(gwc.getResult());
 	}
 }
