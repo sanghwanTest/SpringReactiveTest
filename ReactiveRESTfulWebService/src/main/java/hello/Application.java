8a27cc6e0a9c5c0cf6311bd465eb35494ba2976e
이상환 <@DESKTOP-4J3RAR0>
2018-11-27 23:19:31 +0900

    aaaa add bbbb

--- ReactiveRESTfulWebService/src/main/java/hello/Application.java
+++ ReactiveRESTfulWebService/src/main/java/hello/Application.java
@@ -9,6 +9,8 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 
 		GreetingWebClient gwc = new GreetingWebClient();
+		System.out.println("aaaa");
+		System.out.println("bbbb");
 		System.out.println(gwc.getResult());
 	}
 }
