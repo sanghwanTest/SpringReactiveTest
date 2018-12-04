4bf580c555039d6915eb8212887c66ea47e392d8
이상환 <@DESKTOP-4J3RAR0>
2018-12-05 00:22:47 +0900

    123

--- ReactiveRESTfulWebService/src/main/java/hello/Application.java
+++ ReactiveRESTfulWebService/src/main/java/hello/Application.java
@@ -10,6 +10,8 @@ public class Application {
 
 		GreetingWebClient gwc = new GreetingWebClient();
 		System.out.println("aaa");
+		System.out.println("bbb");
+		System.out.println("ccc");
 		System.out.println(gwc.getResult());
 	}
 }
