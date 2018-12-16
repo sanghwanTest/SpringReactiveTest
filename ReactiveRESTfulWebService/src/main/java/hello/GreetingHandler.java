76f3bf5a07751735074f45c5dcd04eccc52cb9fe
이상환 <@DESKTOP-4J3RAR0>
2018-12-17 00:18:03 +0900

    a

--- ReactiveRESTfulWebService/src/main/java/hello/GreetingHandler.java
+++ ReactiveRESTfulWebService/src/main/java/hello/GreetingHandler.java
@@ -19,7 +19,7 @@ public class GreetingHandler {
 	
 	public Mono<ServerResponse> keys(ServerRequest request){
 		return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON_UTF8)
-				.body(BodyInserters.fromObject("{'resultCode': 'true' or 'false','data' : {'keyName': 'SKT_APP_KEY','appKey' : 'fd986e75-1ef6-4d55-a356-8ca2fea3e156'}}"));
+				.body(BodyInserters.fromObject("{'resultCode': 'true','data' : {'keyName': 'SKT_APP_KEY','appKey' : 'fd986e75-1ef6-4d55-a356-8ca2fea3e156'}}"));
 	}
 	
 }
